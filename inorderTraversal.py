468f412296403f13f7563de3348542a562282c6d
adam <awlapoint@gmail.com>
2025-07-31 02:55:41 -0400

    Iterative version with encoded state in the stack

--- inorderTraversal.py
+++ inorderTraversal.py
@@ -42,16 +42,31 @@ class Solution(object):
         :rtype: List[int]
         """
         result = []
-        def traverse_bt(r_node):
-            if not r_node:
-                return None
+        if not root:
+            return result
 
-            traverse_bt(r_node.left)
-            result.append(r_node.val)
-            traverse_bt(r_node.right)
-            return None
+        stack = []
+        stack.append((root, True, True))
 
-        traverse_bt(root)
+        while stack:
+            current, left, right = stack.pop()
+            if left == True and right == True:
+                stack.append((current, False, True))
+                if current.left:
+                    current = current.left
+                    stack.append((current, True, True))
+                else: # current.left is None
+                    continue
+            elif left == False and right == True:
+                # process current val
+                result.append(current.val)
+                stack.append((current, False, False))
+                if current.right:
+                    current = current.right
+                    stack.append((current, True, True))
+            elif left == False and right == False:
+                continue
+
         return result
 
 
@@ -61,3 +76,4 @@ testcase1 = [1, 2, 3, 4, 5, None, 8, None, None, 6, 7, 9]
 testcase1_pass = [4, 2, 6, 5, 7, 1, 3, 9, 8]
 testcase1_root = build_tree_from_list(testcase1)
 print(solution.inorderTraversal(testcase1_root) == testcase1_pass)
+print(solution.inorderTraversal(testcase1_root))
