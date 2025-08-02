cbc3979e63a31e22e67a7f1f22d7956c776e2467
adam <awlapoint@gmail.com>
2025-08-02 05:41:23 -0400

    Initial working version of isSameTree

--- inorderTraversal.py
+++ inorderTraversal.py
@@ -88,12 +88,45 @@ class Solution(object):
         else:
             return True
 
+    def isSameTree(self, p, q):
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
+        result_p = []
+        result_q = []
+
+        def list_nodes(root, result):
+            if not root:
+                return
+
+            result.append(root.val)
+            if not root.left:
+                result.append("null")
+            else:
+                list_nodes(root.left, result)
+            if not root.right:
+                result.append("null")
+            else:
+                list_nodes(root.right, result)
+
+        list_nodes(p, result_p)
+        list_nodes(q, result_q)
+        return result_p == result_q
+
 
 solution = Solution()
 
-testcase1 = [1, 2, 3, 4, 5, None, 8, None, None, 6, 7, 9]
-testcase1_pass = [4, 2, 6, 5, 7, 1, 3, 9, 8]
-testcase1_root = build_tree_from_list(testcase1)
-print(solution.inorderTraversal(testcase1_root) == testcase1_pass)
-print(solution.inorderTraversal(testcase1_root))
-print(solution.is_tree_balanced(testcase1_root))
+# testcase1 = [1, 2, 3, 4, 5, None, 8, None, None, 6, 7, 9]
+# testcase1_pass = [4, 2, 6, 5, 7, 1, 3, 9, 8]
+# testcase1_root = build_tree_from_list(testcase1)
+# print(solution.inorderTraversal(testcase1_root) == testcase1_pass)
+# print(solution.inorderTraversal(testcase1_root))
+# print(solution.is_tree_balanced(testcase1_root))
+
+p_list = [5,4,1,None,1,None,4,2,None,2]
+q_list = [5,1,4,4,None,1,None,None,2,None,2]
+p = build_tree_from_list(p_list)
+q = build_tree_from_list(q_list)
+print(solution.isSameTree(p, q))
